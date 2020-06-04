be00f3ad77c774e63817a64643a9ccd175dad6f4
Toly Osgood <tosgood@Tolys-MBP.home>
2020-06-04 11:39:52 -0400

    able to scrape tweets for model

--- model.py
+++ model.py
@@ -2,4 +2,22 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 import pickle 
+import GetOldTweets3 as got
+from pandas import DataFrame
+
+username = 'MBTA'
+count = 30
+
+# Creation of query 
+tweetCriteria = got.manager.TweetCriteria().setUsername(username).setMaxTweets(count)
+
+#Tweets into a list
+tweets = []
+for i in range(count):
+    twit = got.manager.TweetManager.getTweets(tweetCriteria)[i]
+    tweets.append(twit.text)
+df = DataFrame(tweets, columns = ['Tweet'])
+print(df)
+
+
 
