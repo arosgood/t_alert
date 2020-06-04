4d5c387367040e2887d4a5d34ce097498735cf1e
Toly Osgood <tosgood@Tolys-MBP.home>
2020-06-03 20:44:44 -0400

    Oauth added + first tweet

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
-import tweepy as tw
+from credentials import *
+import tweepy 
 
-print("test")
+# Set up OAuth and integrate with API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Write a tweet to push to our Twitter account
+tweet = 'Hello, world!'
+api.update_status(status=tweet)
